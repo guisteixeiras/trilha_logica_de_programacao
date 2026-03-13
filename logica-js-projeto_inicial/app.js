7a81d62e345524aaa406f1a560d8dd5a774b7cd6
guisteixeiras <guiteixeira191@icloud.com>
2026-03-13 16:33:12 -0300

    curso 1 finalizado

--- logica-js-projeto_inicial/app.js
+++
@@ -1,9 +0,0 @@
-alert("Boas vindas ao jogo do número secreto!");
-
-let numeroSecreto = 29;
-
-let chute = prompt("Chute um número entre 1 e 30: ");
-
-if (chute == numeroSecreto){
-    console.log("Isso ai! Você descobriu o número secreto (5)");
-}
